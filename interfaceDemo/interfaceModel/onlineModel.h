0a2fc7db11ac9e4f2ab3015be9bf62f15bacd841
CHUXIANWANG <chuxianwang@zhanghaos-Mac-mini.local>
2019-06-14 18:07:42 +0800

    10

---
+++ interfaceDemo/interfaceModel/onlineModel.h
@@ -0,0 +1,13 @@
+//
+//  onlineModel.h
+//  interfaceDemo
+//
+//  Created by CHUXIANWANG on 2019/6/14.
+//  Copyright © 2019年 CHUXIANWANG. All rights reserved.
+//
+
+#ifndef onlineModel_h
+#define onlineModel_h
+
+
+#endif /* onlineModel_h */
---
+++ interfaceDemo/interfaceModel/onlineModel.m
@@ -0,0 +1,9 @@
+//
+//  onlineModel.m
+//  interfaceDemo
+//
+//  Created by CHUXIANWANG on 2019/6/14.
+//  Copyright © 2019年 CHUXIANWANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
